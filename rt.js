731f4a4569aef447d1e14d2362d71986fed002ce
Eddie C. <edimc2000@gmail.com>
2025-02-22 13:46:56 -0600

    commt for practice

---
+++ rt.js
@@ -0,0 +1,2 @@
+const complexArr1 = [{el1: [1,2,3,4,5], el2: [100,200,300]}, {el3: ['c', 'f', 'h', 't', 'y', 'pl', 'ty'], el4: [569,998,547]}];
+const simpleArr= ['c', 'f', 'h', 't', 'y', 'pl', 'ty'];
