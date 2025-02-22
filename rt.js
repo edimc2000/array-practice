f2a5e8f055bc8d8675854361348792db51ee8a9c
ranaabduljabbar <wardjojo_31@yahoo.com>
2025-02-22 14:57:07 -0600

    array-practice

--- rt.js
+++ rt.js
@@ -1,2 +1,32 @@
-const complexArr1 = [{el1: [1,2,3,4,5], el2: [100,200,300]}, {el3: ['c', 'f', 'h', 't', 'y', 'pl', 'ty'], el4: [569,998,547]}];
+const complexArr1 = [
+    {
+        el1: [1,2,3,4,5], 
+        el2: [100,200,300]
+    }, 
+        {
+            el3: ['c', 'f', 'h', 't', 'y', 'pl', 'ty'], 
+            el4: [569,998,547]
+        }];
+
+        console.log(complexArr1 .length) //2
+
+console.log(complexArr1 [0])
+
+console.log(complexArr1[0])
+console.log(complexArr1[0].el1) 
+console.log(complexArr1[0].el1[4]) 
+console.log(complexArr1[0].el1[(complexArr1[0].el1.length-1)])
+
+console.log(complexArr1[1])
+console.log(complexArr1[1].el3)
+console.log(complexArr1[1].el3[3])
+
+
+console.log(complexArr1[1]['el3'][5])
+
+
+/*
 const simpleArr= ['c', 'f', 'h', 't', 'y', 'pl', 'ty'];
+console.log(simpleArr[6])
+console.log(simpleArr[simpleArr.length-1])
+*/
